083834a9db6e2c5834c28c1797d61db0cb6bcf84
aerol457 <58309142+aerol457@users.noreply.github.com>
2020-08-24 09:08:43 +0300

    Add to gitignore

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 const csrfProtection = csrf();
 const fileStorage = multer.diskStorage({
